2628b9ab3e0651f824bd4e31d46f423d07038ba2
Junaid-Razzaq <junaidrazzaq112@gmail.com>
2023-06-14 13:54:49 +0500

    feat:new images added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
 import SearchField from "./shared/components/Home/SearchField";
+import Images from "./shared/assets/page";
 
 export default function Home() {
+  const {
+    ConstructionOne,
+    ConstructionTwo,
+    ConstructionThree,
+    ConstructionFour,
+  } = Images();
   return (
     <main className="">
       <div className="container">
@@ -23,11 +30,38 @@ export default function Home() {
 
         {/* Clients Section */}
         <section className="py-20">
+          <div className="font-serif text-center text-5xl text-primary mb-4">
+            Our Projects
+          </div>
           <div className="grid gap-5 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-            <div>image 1</div>
-            <div>image 2</div>
-            <div>image 3</div>
-            <div>image 4</div>
+            <div>
+              <Image
+                src={ConstructionOne}
+                className="w-auto h-auto"
+                alt="Contruction One"
+              />
+            </div>
+            <div>
+              <Image
+                src={ConstructionTwo}
+                className="w-auto h-auto"
+                alt="Contruction Two"
+              />
+            </div>
+            <div>
+              <Image
+                src={ConstructionThree}
+                className="w-auto h-auto"
+                alt="Contruction Three"
+              />
+            </div>
+            <div>
+              <Image
+                src={ConstructionFour}
+                className="w-auto h-auto"
+                alt="Contruction Four"
+              />
+            </div>
           </div>
         </section>
       </div>
--- src/app/shared/assets/page.ts
+++ src/app/shared/assets/page.ts
@@ -1,6 +1,18 @@
 import ImageOne from './image1.jpeg'
+import ConstructionOne from './construction.jpg'
+import ConstructionTwo from './construction2.jpg'
+import ConstructionThree from './construction3.jpg'
+import ConstructionFour from './construction4.jpg'
 
-const Images =() => {
-    ImageOne : ImageOne
+const Images = () => {
+    return {
+        ImageOne : ImageOne,
+        ConstructionOne : ConstructionOne,
+        ConstructionTwo : ConstructionTwo,
+        ConstructionThree : ConstructionThree,
+        ConstructionFour : ConstructionFour,
+    
+}
+
 }
 export default Images
